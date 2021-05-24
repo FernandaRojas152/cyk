e64d84c0f343be3e4858238194141fa023bc6c10
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2021-05-24 17:01:59 -0500

    Uribe implanto un falso positivo aquí

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,17 +6,37 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-
 public class Main extends Application {
+	private MenuController menu;
+	
 	@Override
-	public void start(Stage stage) throws Exception{
-		 Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
-	        stage.setScene(new Scene(root));
-	        stage.setResizable(false);
-	        stage.show();
+	public void start(Stage primaryStage) throws Exception{
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+			Parent root = fxmlLoader.load();
+			menu = fxmlLoader.getController();
+			setMenu(fxmlLoader.getController());
+			Scene scene= new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/resources/MainMenu.css").toExternalForm());
+			primaryStage.setScene(scene);
+			//primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+			primaryStage.setTitle("cyk algorithm");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public MenuController getMenu() {
+		return menu;
+	}
+
+	public void setMenu(MenuController menu) {
+		this.menu = menu;
 	}
 }
